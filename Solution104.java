8912927c03762f0bb56b50c37b717aa8c95d63c0
Vinay <vinay1020287@gmail.com>
2021-08-26 14:42:25 -0400

    Maximum Depth of Binary Tree

---
+++ Solution104.java
@@ -0,0 +1,9 @@
+public class Solution104 {
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    } else {
+      return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+  }
+}
