73259e6c079c40235559954f24a612d06d4f3a30
Vinay <vinay1020287@gmail.com>
2021-09-27 21:25:28 -0400

    Right side view of a binary tree

---
+++ Solution199.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+}
+ 
+class Solution199 {
+  public List<Integer> rightSideView(TreeNode root) {
+    ArrayList<Integer> rsv = new ArrayList<>();
+    if(root==null){
+      return rsv;
+    }
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    while(!q.isEmpty()){
+      int levelSize = q.size();
+      while(levelSize > 0){
+        TreeNode current = q.poll();
+
+        if(current.left != null){
+          q.add(current.left);
+        }
+
+        if(current.right != null){
+          q.add(current.right);
+        }
+
+        if(levelSize-- == 1){
+          rsv.add(current.val);
+        }
+      }
+    }
+    return rsv;
+  }
+}
