377fcd5d07f39a5d825b60e2fc8cbd0da78c1f7f
Vinay <vinay1020287@gmail.com>
2021-09-28 14:31:42 -0400

    Reverse vowels in a string

--- Solution199.java
+++ Solution199.java
@@ -4,8 +4,18 @@ import java.util.List;
 import java.util.Queue;
 
 
-
-
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+}
  
 class Solution199 {
   public List<Integer> rightSideView(TreeNode root) {
