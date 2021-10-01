9af0f26f53d2846e1fcdb9a38654bd53d8ef229a
Vinay <vinay1020287@gmail.com>
2021-09-30 20:23:02 -0400

    Range sum of BST

---
+++ Solution938.java
@@ -0,0 +1,17 @@
+class Solution938 {
+  int sum = 0;
+  public int rangeSumBST(TreeNode root, int low, int high) {
+    if(root == null){
+      return 0;
+    }
+
+    if(root.val>=low && root.val<=high){
+      sum+=root.val;
+    }
+
+    rangeSumBST(root.right, low, high);
+    rangeSumBST(root.left, low, high);
+
+    return sum;
+  }
+}
